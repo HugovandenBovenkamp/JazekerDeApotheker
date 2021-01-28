c83c086139b8a6b18af31d156b8b7326074bb9e5
Hugo van den Bovenkamp <hugovdbovenkamp@gmail.com>
2021-01-28 19:40:40 +0100

    Database connectie werkend, templatestructuur html aangemaakt

---
+++ includes/dbconnect.php
@@ -0,0 +1,14 @@
+<?php
+$con = mysqli_connect("localhost","root","", "apotheek");
+
+//check connection
+
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to Database: " . mysqli_connect_error();
+    exit();
+}
+else {
+    echo "Database connected!";
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+   include("includes/dbconnect.php");
+   include("includes/head.php");
+  ?>
+
+
+  <h1>Hello, world!</h1>
+
+  
+  <?php
+    include("includes/footer.php");
+  ?>
+</html>
