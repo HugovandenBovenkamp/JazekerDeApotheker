df2e703eec8b5eaa1d2fbe09bf976146745e3f52
Erwin Venneker <erwinvenneker13@gmail.com>
2021-01-29 17:11:22 +0100

    added mediaqueries

--- index.php
+++ index.php
@@ -25,6 +25,9 @@
     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2406.8625226715694!2d4.7493073163410395!3d52.89689871598028!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47cf4883321d7567%3A0x9344edc0760d6d5c!2sAlphega%20apotheek%20Julianadorp!5e0!3m2!1snl!2snl!4v1611930859827!5m2!1snl!2snl" width="100%" height="100%" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
     </div>
   </div>
+  <div class="row">
+    
+  </div>
 </div>
 
 <?php
