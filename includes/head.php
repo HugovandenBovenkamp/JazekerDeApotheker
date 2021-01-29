12964515d151a7bb9005391c86d53480603f20a1
Hugo van den Bovenkamp <hugovdbovenkamp@gmail.com>
2021-01-29 16:25:00 +0100

    Updated navbar

--- includes/head.php
+++ includes/head.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;700&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link rel="stylesheet" href="css/main.css">
@@ -19,8 +20,15 @@
                 <div class="offset-sm-1 col-sm-11">
                     <a href=""><img src="assets/logo apotheek website.png" alt="logo"></a>
                 </div>
-                <div class="row">
-                    
+            </div>
+            <div class="row">
+                <div class="col-sm-10 offset-sm-1">
+                    <nav class="d-flex justify-content-around">
+                        <a href="">Home</a>
+                        <a href="">Service</a>
+                        <a href="">Voorlichting</a>
+                        <a href="">Mijn APO</a>
+                    </nav>
                 </div>
             </div>
         </div>
