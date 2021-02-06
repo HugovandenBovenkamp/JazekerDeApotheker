adf6f0f906674e3d179d9cc37b6c1ff5226cd863
erwinve <erwinvenneker13@gmail.com>
2021-02-06 22:25:46 +0100

    adjustments css

--- index.php
+++ index.php
@@ -31,44 +31,56 @@
       <div class="home-blocks-group">
         <div class="row">
           <div class="col-xs-12 col-sm-4">
-            <div class="home-block block1">
-              <h3>Herhaalservice</h3>
-            </div>
+            <a href="">
+              <div class="home-block block1">
+                <h3>Herhaalservice</h3>
+              </div>
+            </a>
           </div>
           <div class="col-xs-12 col-sm-4">
-            <div class="home-block block2">
-              <h3>Medicijneoverzicht</h3>
-            </div>
+            <a href="">
+              <div class="home-block block2">
+                <h3>Medicijneoverzicht</h3>
+              </div>
+            </a>
           </div>
           <div class="col-xs-12 col-sm-4">
-            <div class="home-block block3">
-              <h3>Inschrijven</h3>
-            </div>
+            <a href="">
+              <div class="home-block block3">
+                <h3>Inschrijven</h3>
+              </div>
+            </a>
           </div>
         </div>
       </div>
 
-      <div class="row">
+      <div class="row row-eq-height">
         <div class="col-xs-12 col-sm-4">
-          <h3>Openingstijden</h3>
-          <ul>
-            <li>Maandag: 8:00 - 17:00</li>
-            <li>Dinsdag: 8:00 - 17:00</li>
-            <li>Woensdag: 8:00 - 17:00</li>
-            <li>Donderdag: 8:00 - 17:00</li>
-            <li>Vrijdag: 8:00 - 17:00</li>
-            <li>Zaterdag 8:00 - 15:00</li>
-            <li>Zondag: Gesloten</li>
-          </ul>
+          <div class="item-border">
+            <h3>Openingstijden</h3>
+            <ul>
+              <li>Maandag: 8:00 - 17:00</li>
+              <li>Dinsdag: 8:00 - 17:00</li>
+              <li>Woensdag: 8:00 - 17:00</li>
+              <li>Donderdag: 8:00 - 17:00</li>
+              <li>Vrijdag: 8:00 - 17:00</li>
+              <li>Zaterdag 8:00 - 15:00</li>
+              <li>Zondag: Gesloten</li>
+            </ul>
+          </div>
         </div>
         <div class="col-xs-12 col-sm-6 col-sm-offset-1">
-          <h3>Adres</h3>
+          <div class="item-border">
+            <h3>Adres</h3>
+          </div>
         </div>
       </div>
 
       <div class="row">
         <div class="col-xs-12">
-          <h3>Nieuws</h3>
+          <div class="item-border">
+            <h3>Nieuws</h3>
+          </div>
         </div>
       </div>
     </div>
