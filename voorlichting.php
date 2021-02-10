6105e1bdce68e43279e7c2ef4658178b820b9e25
Hugo van den Bovenkamp <hugovdbovenkamp@gmail.com>
2021-02-10 13:38:25 +0100

    Made voorlichting page, added styling

---
+++ voorlichting.php
@@ -0,0 +1,33 @@
+<?php
+include("includes/head.php");
+?>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="voorlichting-content col-10 offset-1 border border-dark">
+            <div>
+                <h3>Gezondheidsinformatie</h3>
+                <p>In samenwerking met <a href="https://www.thuisarts.nl/">Thuisarts.nl</a> bieden wij u
+                    gezondheidsinformatie.</p>
+                <p>Wilt u meer weten over een aandoening, zoekt u dan op de naam in de onderstaande zoeklijst.</</p>
+                    <div class="aandoening-lijst">
+                        <h3>Aandoeningen</h3>
+
+                        <!-- database lijst met aandoeningen hier -->
+                            <ul>
+                                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                            </ul>
+                    </div>     
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+include("includes/footer.php");
+?>
