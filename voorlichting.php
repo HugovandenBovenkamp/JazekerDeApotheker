39f2f7f823dc124465704c9de9366cad9743417b
Hugo van den Bovenkamp <hugovdbovenkamp@gmail.com>
2021-02-11 09:59:01 +0100

    Styled buttons for voorlichting page

--- voorlichting.php
+++ voorlichting.php
@@ -5,13 +5,13 @@ include("includes/head.php");
 <div class="container-fluid">
     <div class="row">
         <div class="voorlichting-content col-10 offset-1 border border-dark">
-                <h3>Gezondheidsinformatie</h3>
-                <p>In samenwerking met <a href="https://www.thuisarts.nl/">Thuisarts.nl</a> bieden wij u
-                    gezondheidsinformatie.</p>
-                <p>Wilt u meer weten over een aandoening, zoekt u dan op de naam in de onderstaande zoeklijst.</</p>
-        <div class="flex-container">   
-            
-            <div class="aandoening-lijst">
+            <h3>Gezondheidsinformatie</h3>
+            <p>In samenwerking met <a href="https://www.thuisarts.nl/">Thuisarts.nl</a> bieden wij u
+                gezondheidsinformatie.</p>
+            <p>Wilt u meer weten over een aandoening, zoekt u dan op de naam in de onderstaande zoeklijst.</</p> 
+           
+                <!-- Aandoening lijst -->
+                <div class="aandoening-lijst">
                     <h3>Aandoeningen</h3>
                     <div class="input-section">
                         <input type="text">
@@ -25,25 +25,24 @@ include("includes/head.php");
                         <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
                         <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
                     </ul>
-            </div>
-
-            <div class="medicijnen-lijst">
-                <h3>Medicijnen</h3>
-                <div class="input-section">
-                    <input type="text">
-                    <button class="zoek-button">Zoek</button>
                 </div>
-                <ul>
-                    <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
-                    <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
-                    <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
-                    <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
-                    <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
-                    <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
-                </ul>
-            </div>
-            
-        </div>
+
+                <!-- Medicijnen lijst -->
+                <div class="medicijnen-lijst">
+                    <h3>Medicijnen</h3>
+                    <div class="input-section">
+                        <input type="text">
+                        <button class="zoek-button">Zoek</button>
+                    </div>
+                    <ul>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                    </ul>
+          </div>
     </div>
 </div>
 </div>
